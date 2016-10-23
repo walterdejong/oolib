f9a671cca061bb0c5a8324b27740ebb2af6f0d6f
Walter de Jong <walter@heiho.net>
2016-10-23 13:29:22 +0200

    no more swap()

--- include/oo/Base.h
+++ include/oo/Base.h
@@ -6,13 +6,13 @@
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met: 
+ * modification, are permitted provided that the following conditions are met:
  *
  * 1. Redistributions of source code must retain the above copyright notice, this
- *    list of conditions and the following disclaimer. 
+ *    list of conditions and the following disclaimer.
  * 2. Redistributions in binary form must reproduce the above copyright notice,
  *    this list of conditions and the following disclaimer in the documentation
- *    and/or other materials provided with the distribution. 
+ *    and/or other materials provided with the distribution.
  *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
@@ -60,12 +60,10 @@ std::ostream& operator<<(std::ostream& os, const __classname__& t) {
 
 /*
 	an object is deleted by swapping it with an empty instance
-	This requires method swap(a,b)
 */
 template <class T>
 void del(T& a) {
-	T tmp;
-	std::swap(a, tmp);
+	a = std::move(T());
 }
 
 /*
